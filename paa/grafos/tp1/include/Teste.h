b6052def7238b7dfbdbf11c285d8218f46109402
Alberto Ueda <albertoueda@gmail.com>
2014-10-03 01:47:09 -0300

    templates codeblocks

---
+++ paa/grafos/tp1/include/Teste.h
@@ -0,0 +1,14 @@
+#ifndef TESTE_H
+#define TESTE_H
+
+
+class Teste
+{
+    public:
+        Teste(a);
+        virtual ~Teste();
+    protected:
+    private:
+};
+
+#endif // TESTE_H
---
+++ paa/grafos/tp1/src/Teste.cpp
@@ -0,0 +1,11 @@
+#include "Teste.h"
+
+Teste::Teste(a)
+{
+    //ctor
+}
+
+Teste::~Teste()
+{
+    //dtor
+}
