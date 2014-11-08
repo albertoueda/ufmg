c7522a62e6b075b46df373b2ef450bb4d76b833e
Alberto Ueda <albertoueda@gmail.com>
2014-11-08 17:46:32 -0200

    TP3 - Finished Brute Force Algorithm

--- paa/grafos/tp3/alberto-ueda-tp3.cpp
+++ paa/grafos/tp3/alberto-ueda-tp3.cpp
@@ -21,6 +21,8 @@ Student ID: 2014765817
 #define BOTH "both"
 #define COMMAND_1 "11111"
 #define COMMAND_0 "000"
+#define SPECIAL_JOKER_1 "1111-00"
+#define SPECIAL_JOKER_2 "00-1111"
 
 using namespace std;
 
@@ -35,30 +37,79 @@ bool find_command(string message)
     return false;
 }
 
+bool find_special_joker(string message)
+{
+    if (message.find(SPECIAL_JOKER_1) != string::npos || message.find(SPECIAL_JOKER_2) != string::npos)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void generate_all_possibilities(string new_message, vector<string>* all_possibilities)
+{
+    int joker_index = new_message.find('-');
+
+    if (joker_index  == string::npos) {
+        // cout << "New possibility: " << new_message << endl;
+        (*all_possibilities).push_back(new_message);
+    }
+    else
+    {
+        string new_message_with_1 = new_message;
+
+        new_message[joker_index] = '0';
+        generate_all_possibilities(new_message, all_possibilities);
+
+        new_message_with_1[joker_index] = '1';
+        generate_all_possibilities(new_message_with_1, all_possibilities);
+    }
+
+}
 
 string brute_force(string message)
 {
     // check if message has a control command, disconsidering errors
-    if (find_command(message))
+    if (find_command(message) || find_special_joker(message))
     {
         return TRUE;
     }
 
+    if (message.find('-') == string::npos) {
+        return FALSE;
+    }
+
     for (int i = 0; i < message.size(); ++i)
     {
+        // cout << "    i = " << i << endl;
+        if (message.find('-') == string::npos) {
+            continue;
+        }
 
+        string submessage = message.substr(i, 5);
 
-        // generate all possible solutions for interval of 7
+        vector<string> all_possible_messages;
+        all_possible_messages.reserve(pow(2, 5));
+        generate_all_possibilities(submessage, &all_possible_messages);
 
-        // check each solution
-
-        // go to next interval (step size: 1)
+        for (int k = 0; k < all_possible_messages.size(); ++k)
+        {
+            if (find_command(all_possible_messages[k]))
+            {
+                return BOTH;
+            }
+        }
 
     }
 
     return FALSE;
 }
 
+string greedy(string message)
+{
+    return FALSE;
+}
 
 void print_nodes(const char* filename, vector<int>* nodes)
 {
@@ -103,7 +154,8 @@ int main(int argc, char** argv)
         string instance;
         getline(inputFile, instance);
 
-        cout << brute_force(instance) << endl;
+        cout << greedy(instance) << endl;
+        // cout << brute_force(instance) << endl;
     }
 
     inputFile.close();
